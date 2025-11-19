079fccbe91139e6a58d5dfdf7424ae046c494ee4
YongSoekChoeng <jysn007@gmail.com>
2025-11-19 22:09:08 +0900

    .

--- src/main/java/net/dstone/batch/sample/jobs/job003/TableUpdateJob.java
+++ src/main/java/net/dstone/batch/sample/jobs/job003/TableUpdateJob.java
@@ -1,13 +1,18 @@
 package net.dstone.batch.sample.jobs.job003;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.mybatis.spring.batch.MyBatisBatchItemWriter;
+import org.mybatis.spring.batch.MyBatisPagingItemReader;
+import org.mybatis.spring.batch.builder.MyBatisPagingItemReaderBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.SynchronizedItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -28,12 +33,28 @@ public class TableUpdateJob extends AbstractJob {
 	public void configJob() throws Exception {
 		log(this.getClass().getName() + ".configJob() has been called !!!");
 		int chunkSize = 10;
-		this.addStep(this.createMultiThreadStep("01.멀티쓰레드스텝1", chunkSize));
+		//this.addStep(this.createStepByOperator("01.Reader/Processor/Writer 별도클래스로 생성 스텝", chunkSize));
+		this.addStep(this.createStepInAll("02.Reader/Processor/Writer 동일클래스내에 생성 스텝", chunkSize));
 	}
 
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("batch-thread-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.initialize();
+        return executor;
+    }
+
+    
+    /**************************************** 01.Reader/Processor/Writer 별도클래스로 생성 ****************************************/
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private Step createMultiThreadStep(String stepName, int chunkSize) {
-		log(this.getClass().getName() + ".createMultiThreadStep("+stepName+", "+chunkSize+" ) has been called !!!");
+	private Step createStepByOperator(String stepName, int chunkSize) {
+		log(this.getClass().getName() + ".createStepByOperator("+stepName+", "+chunkSize+" ) has been called !!!");
 		return new StepBuilder(stepName, jobRepository)
 				.<Map, Map>chunk(chunkSize, txManagerCommon)
 				.reader(itemReader())
@@ -60,17 +81,56 @@ public class TableUpdateJob extends AbstractJob {
     public ItemWriter<Map<String, Object>> itemWriter() {
         return new TableUpdateWriter(this.sqlSessionSample);
     }
+    /*************************************************************************************************************************/
+    
+    /*************************************** 02.Reader/Processor/Writer 동일클래스내에 생성 ***************************************/
+    public Step createStepInAll(String stepName, int chunkSize) {
+        return new StepBuilder(stepName, jobRepository)
+                .<Map<String, Object>, Map<String, Object>>chunk(chunkSize, txManagerCommon)
+                .reader(tableUpdateReader(chunkSize))
+                .processor(tableUpdateProcessor())
+                .writer(tableUpdateWriter())
+                .taskExecutor(taskExecutor())
+                .build();
+    }
 
     @Bean
-    public TaskExecutor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(5);
-        executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("batch-thread-");
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.setAwaitTerminationSeconds(60);
-        executor.initialize();
-        return executor;
+    @StepScope
+    public MyBatisPagingItemReader<Map<String, Object>> tableUpdateReader(int chunkSize) {
+        Map<String, Object> params = new HashMap<>();
+        // 필요시 파라미터 추가 가능
+        return new MyBatisPagingItemReaderBuilder<Map<String, Object>>()
+                .sqlSessionFactory(this.sqlSessionSample.getSqlSessionFactory())
+                .queryId("net.dstone.batch.sample.SampleTestDao.selectListSampleTestPaging")
+                .parameterValues(params)
+                .pageSize(chunkSize)
+                .build();
     }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<Map<String, Object>, Map<String, Object>> tableUpdateProcessor() {
+        return item -> {
+            // 데이터 수정 로직
+            String testId = (String) item.get("TEST_ID");
+            String testName = (String) item.get("TEST_NAME");
+            
+            // FLAG_YN을 'Y'로 변경
+            item.put("FLAG_YN", "Y");
+            
+            // 수정된 데이터 반환
+            return item;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public ItemWriter<Map<String, Object>> tableUpdateWriter() {
+        MyBatisBatchItemWriter<Map<String, Object>> writer = new MyBatisBatchItemWriter<>();
+        writer.setStatementId("net.dstone.batch.sample.SampleTestDao.updateSampleTest");
+        writer.setSqlSessionFactory(this.sqlSessionSample.getSqlSessionFactory());
+        return new SynchronizedItemWriter<>(writer);
+    }
+    /*************************************************************************************************************************/
+
 }
