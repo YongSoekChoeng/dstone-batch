83e56f92cfbf8ac3fd8995da6e60b44b1cc10adc
YongSoekChoeng <jysn007@gmail.com>
2025-11-21 13:30:17 +0900

    .

--- src/main/java/net/dstone/batch/sample/jobs/job002/TableInsertTaskletJob.java
+++ src/main/java/net/dstone/batch/sample/jobs/job002/TableInsertTaskletJob.java
@@ -1,8 +1,5 @@
 package net.dstone.batch.sample.jobs.job002;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import net.dstone.batch.common.annotation.AutoRegJob;
--- src/main/java/net/dstone/batch/sample/jobs/job003/TableUpdateJob.java
+++ src/main/java/net/dstone/batch/sample/jobs/job003/TableUpdateJob.java
@@ -13,7 +13,6 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
