88b2a6bc810ab6654830c2167487bcb7ccfd4ef4
YongSoekChoeng <jysn007@gmail.com>
2025-12-10 21:48:54 +0900

    .

--- src/main/java/net/dstone/batch/common/config/ConfigListener.java
+++ src/main/java/net/dstone/batch/common/config/ConfigListener.java
@@ -2,6 +2,7 @@ package net.dstone.batch.common.config;
 
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,13 +40,45 @@ public class ConfigListener extends BaseBatchObject {
             		buff.append("\n");
             	}
             	buff.append("\n");
+            	if( !"STARTED".equals(jobStatus) ) {
+            		buff.append(resultCountLog(jobExecution));
+            	}
             	buff.append("||======================================= Job["+jobName+"] "+ jobStatus +" =======================================||");
             	if( !"STARTED".equals(jobStatus) ) {
             		buff.append("\n");
             	}
-            	
             	info(buff.toString());
             }
+            
+            private String resultCountLog(JobExecution jobExecution) {
+            	StringBuffer jobBuff = new StringBuffer();
+            	String jobName = jobExecution.getJobInstance().getJobName();
+            	jobBuff.append("\n").append("<").append(jobName).append(" 수행결과>").append("\n");
+            	long readCnt = 0l;
+            	long writeCnt = 0l;
+            	long skipCnt = 0l;
+            	StringBuffer buff = new StringBuffer();
+                for (StepExecution stepExecution : jobExecution.getStepExecutions()) {
+                	buff.append("------------------------------------------------------").append("\n");
+                	buff.append("Step:").append(stepExecution.getStepName()).append("\n");
+                	buff.append("READ건수:").append(stepExecution.getReadCount()).append("\n");
+                	buff.append("WRITE(성공)건수:").append(stepExecution.getWriteCount()).append("\n");
+                	buff.append("SKIP(실패)건수:").append(stepExecution.getSkipCount()).append("\n");
+                	buff.append("Status:").append(stepExecution.getStatus()).append("\n");
+                	// Partition 인 경우 Master Step에 수치가 있으므로 총 건수에서 건너뛴다.
+                	if( stepExecution.getStepName().indexOf(":") == -1 ) {
+                    	readCnt = readCnt + stepExecution.getReadCount();
+                    	writeCnt = writeCnt + stepExecution.getWriteCount();
+                    	skipCnt = skipCnt + stepExecution.getSkipCount();
+                	}
+                }
+            	buff.append("------------------------------------------------------").append("\n");
+            	
+            	jobBuff.append("총 READ건수:").append(readCnt).append(", 총 WRITE(성공)건수:").append(writeCnt).append(", 총 SKIP(실패)건수:").append(skipCnt).append("\n");
+            	jobBuff.append(buff);
+            	return jobBuff.toString();
+            }
+            
     	};
     }
 
